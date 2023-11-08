58a4882e077072988ef68edfd67a551f41f06d4b
suzaku <89909040+suzaku01@users.noreply.github.com>
2023-11-08 11:58:01 +0900

    update

---
+++ genMhfKey_jpn.py
@@ -0,0 +1,73 @@
+import os
+import zlib
+import sys
+import hashlib
+from multiprocessing import Pool
+
+
+def sha256(file):
+    with open(file.filename, 'rb') as fp:
+        s = fp.read()
+        h = hashlib.sha256()
+        h.update(s)
+    return h.hexdigest().upper()
+
+
+def crc32(file):
+    with open(file.filename, 'rb') as fp:
+        h = 0
+        while True:
+            s = fp.read(65536)
+            if not s:
+                break
+            h = zlib.crc32(s, h)
+    return "%08X" % (h & 0xFFFFFFFF)
+
+
+def filetime(file):
+    return int((os.path.getmtime(file.filename) * 10000000) + 116444736000000000).to_bytes(8, 'big')
+
+
+def fileinfo(file, target):
+    if target == '51':
+        filehash = sha256(file)
+    else:
+        filehash = crc32(file)
+    return filehash, filetime(file), file.filename, file.size
+
+
+class MhfFile:
+    def __init__(self, _filename, _size):
+        self.filename = _filename
+        self.size = _size
+
+
+if __name__ == '__main__':
+    target = ''
+    if len(sys.argv) == 2:
+        target = sys.argv[1]
+    else:
+        target = 'JP'
+
+    if target == '51':
+        print('Targeting PS3, using SHA256')
+    else:
+        print('Targeting PC, using CRC32')
+    key = set()
+    files = set()
+    for r, _, f in os.walk('jpndat', topdown=False):
+        for name in f:
+            fn = os.path.join(r, name)
+            fs = os.stat(fn).st_size
+            if fs == 0:
+                continue
+            files.add(MhfFile(fn, fs))
+
+    with Pool() as pool:
+        infos = pool.starmap(fileinfo, [(file, target) for file in files])
+        for crc, filetime, filename, size in infos:
+            dfn = filename.replace('/', '\\')[7:]
+            key.add(f'{crc},{filetime[4:8].hex().upper()},{filetime[0:4].hex().upper()},{dfn},{size},0\n')
+
+    with open(f'key{target}.txt', 'w') as fp:
+        fp.write(''.join(key))
--- mhf_file.php
+++ mhf_file.php
@@ -1,16 +1,28 @@
 <?php
 
 if (isset($_GET['key'])) {
-	$platform = "00";
-	// PS3 override stub
-	if ($_SERVER['HTTP_HOST'] == 'ps3-en.zerulight.cc' || $_SERVER['HTTP_HOST'] == 'ps3-jp.zerulight.cc') {
-		$platform = "51";
-	}
-	header(sprintf('Content-Disposition: inline; filename="MHFUP_%s.DAT"', $platform));
-	header('Content-Length: '.filesize(sprintf('key%s.txt', $platform)));
-	header('Connection: close');
-	header('Content-Type: application/octet-stream');
-	readfile(sprintf('key%s.txt', $platform));
+    $platform = "00"; // デフォルトのプラットフォームコード
+	$zero = "00";
+    $userAgent = $_SERVER['HTTP_USER_AGENT']; // ユーザーエージェントの値を取得
+
+    // 日本語版クライアントからのリクエストをチェック
+    if (strpos($userAgent, "Monster Hunter Frontier Online Launcher Release Jpn.  1.500") !== false) {
+        $platform = "JP"; // 日本語版用のプラットフォームコードに設定
+    }
+
+    // 英語版クライアントからのリクエストをチェック
+    if (strpos($userAgent, "Monster Hunter Frontier Online Launcher Release Eng.  ENG") !== false) {
+        $platform = "EN"; // 英語版用のプラットフォームコードに設定
+    }
+
+    // ファイル名を指定してコンテンツを送信
+    $filename = sprintf('MHFUP_%s.DAT', $zero);
+    $filepath = sprintf('key%s.txt', $platform);
+    header('Content-Disposition: inline; filename="' . $filename . '"');
+    header('Content-Length: ' . filesize($filepath));
+    header('Connection: close');
+    header('Content-Type: application/octet-stream');
+    readfile($filepath);
 }
 
 if (isset($_GET['chk'])||isset($_GET['chksha'])) {
